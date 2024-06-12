a9a2740334eeff783fec4bba3327dc4090893137
Daniel Dib <dan-dib@hotmail.com>
2024-06-12 03:39:58 -0300

    Projeto finalizado

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.print("Digite o nome do cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Digite o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Digite o número da agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o saldo disponível na conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numeroConta +" e seu saldo "+ saldo +" já está disponível para saque");
+
+        scanner.close();
+    }
 }
